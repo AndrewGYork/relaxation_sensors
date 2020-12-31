112cf9d947bdb1b4c2d396e11025617c2c7e1538
Andrew York <andrew.g.york+github@gmail.com>
2020-12-30 18:25:24 -0800

    Update autofluorescence_figure.py

--- autofluorescence_figure.py
+++ autofluorescence_figure.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from tifffile import imread, imwrite # v2020.6.3 or newer
 
 input_dir = Path.cwd()
@@ -21,13 +22,14 @@ def main():
     data = imread(input_dir / '0_data.tif') # Acquired by Maria on 2020_12_01
     print(data.shape, data.dtype)
     
-    timestamps = decode_timestamps(data)['microseconds']
+    timestamps = decode_timestamps(data)['microseconds'].astype('float64')
     
     # Crop, flip, convert to float:
     data = data[:, 1776:162:-1, 1329:48:-1].transpose((0, 2, 1)).astype('float32')
     print(data.shape, data.dtype)
 
-    # Extract the relevant images: minimum and maximum photoactivation
+    # Extract the relevant images to separate signal from background:
+    # minimum and maximum photoactivation
     deactivated = data[(5, 21, 37), :, :] # The dimmest image(s)
     imwrite(temp_dir / '1_deactivated.tif', deactivated, imagej=True)
     activated = data[(14, 30, 46), :, :] # The brightest image(s)
@@ -87,7 +89,7 @@ def main():
                 fontdict={'color': (1, 0, 1),
                           'weight': 'bold',
                           'size': 4})
-        plt.savefig(temp_dir / ("animation_frame_%03i.png"%i), dpi=800)
+        plt.savefig(temp_dir / ("overlay_frame_%03i.png"%i), dpi=800)
 
     # Animate the frames into a gif:
     palette = temp_dir / "palette.png"
@@ -96,17 +98,17 @@ def main():
     convert_command_1 = [
         'ffmpeg',
         '-f', 'image2',
-        '-i', str(temp_dir / 'animation_frame_%3d.png'),
+        '-i', str(temp_dir / 'overlay_frame_%3d.png'),
         '-vf', filters + ",palettegen",
         '-y', palette]
     convert_command_2 = [
         'ffmpeg',
         '-framerate', '0.7',
         '-f', 'image2',
-        '-i', str(temp_dir / 'animation_frame_%3d.png'),
+        '-i', str(temp_dir / 'overlay_frame_%3d.png'),
         '-i', palette,
         '-lavfi', filters + " [x]; [x][1:v] paletteuse",
-        '-y', str(output_dir / "animation.gif")]
+        '-y', str(output_dir / "1_overlay_animation.gif")]
     for convert_command in convert_command_1, convert_command_2:
         try:
             with open(temp_dir / 'conversion_messages.txt', 'wt') as f:
@@ -119,6 +121,122 @@ def main():
             print("GIF conversion failed. Is ffmpeg installed?")
             raise
     print('done.')
+
+    # Extract images for one photoswitching cycle. Smooth them a little,
+    # since we'll be resampling the image anyway for the figure.
+    cycle = gaussian_filter(data[4:15, :, :], sigma=(0, 1, 1))
+    cycle_timestamps = (timestamps[4:15] - timestamps[5]) * 1e-6
+    imwrite(temp_dir / '8_switching_cycle.tif',
+            np.expand_dims(cycle, 1), imagej=True)
+
+    # Output annotated png for animation.
+    for i in range(cycle.shape[0]):
+        fig = plt.figure(figsize=(8, 8*(cycle.shape[1]/cycle.shape[2])))
+        ax1 = plt.axes([0, 0, 1, 1])
+        ax1.imshow(cycle[i], interpolation='nearest', cmap=plt.cm.gray,
+                  vmin=100, vmax=625)
+        ax1.set_xticks([])
+        ax1.set_yticks([])
+        ax1.text(
+            1200, 650,
+            "t=%5ss"%('%0.2f'%(cycle_timestamps[i])),
+            fontdict={'color': (1, 1, 1),
+                      'weight': 'bold',
+                      'size': 20,
+                      'family': 'monospace',})
+        ax2 = plt.axes([0.4, 0.095, 0.57, 0.35])
+        ax1.add_patch(Rectangle(
+            (279, 789), 21, 21,
+            fill=False, linewidth=3, linestyle=(0, (0.5, 0.5)),
+            color=(1, 0, 1)))
+        ax1.add_patch(Rectangle(
+            (722, 376), 21, 21,
+            fill=False, linewidth=2, color=(0, 1, 0)))
+        box_1_photons = to_photoelectrons(
+            cycle[:, 789:789+21, 279:279+21].mean(axis=(1, 2)))
+        box_2_photons = to_photoelectrons(
+            cycle[:, 376:376+21, 722:722+21].mean(axis=(1, 2)))
+        ax2.plot(cycle_timestamps, box_1_photons,
+                 marker='.', markersize=7,
+                 linewidth=2.5, linestyle=(0, (1, 1)),
+                 color=(1, 0, 1))
+        ax2.plot(cycle_timestamps, box_2_photons,
+                 marker='.', markersize=7,
+                 linewidth=2.5,
+                 color=(0, 1, 0))
+        ax2.plot([cycle_timestamps[-1]*1.08]*2,
+                 [box_1_photons.min(), box_1_photons.max()],
+                 marker=0, markersize=10, linewidth=2.5,
+                 color=(1, 0, 1))
+        ax2.plot([cycle_timestamps[-1]*1.12]*2,
+                 [box_2_photons.min(), box_2_photons.max()],
+                 marker=0, markersize=10, linewidth=2.5,
+                 color=(0, 1, 0))
+        ax2.text(
+            3.79, 93,
+            "%0.1f%%"%(100 * (box_1_photons.max() - box_1_photons.min()) /
+                       box_1_photons.min()),
+            fontdict={'color': (1, 0, 1),
+                      'weight': 'bold',
+                      'size': 10,})
+        ax2.text(
+            3.95, 65,
+            "%0.1f%%"%(100 * (box_2_photons.max() - box_2_photons.min()) /
+                       box_2_photons.min()),
+            fontdict={'color': (0, 0.5, 0),
+                      'weight': 'bold',
+                      'size': 10,})
+        ax2.set_xlim(cycle_timestamps.min() - 0.1, 1.35*cycle_timestamps.max())
+        ax2.set_ylim(0, 1.2*max(box_1_photons.max(), box_2_photons.max()))
+        ax2.grid('on')
+        ax2.tick_params(labelcolor='white')
+        ax2.set_xlabel("Time (s)", color='white', weight='bold')
+        ax2.set_ylabel("Photons per pixel", color='white', weight='bold')
+        ax2.axvline(cycle_timestamps[i])
+        ax2.add_patch(Rectangle(
+            (cycle_timestamps[0] - 0.1, 0),
+            (cycle_timestamps[1] - cycle_timestamps[0]) - 0.1, 130,
+            fill=True, linewidth=0, color=(0, 0, 1, 0.17)))
+        ax2.text(
+            0.7 + cycle_timestamps[0], 22,
+            "405 nm\nillumination",
+            fontdict={'color': (0, 0, 1),
+                      'weight': 'bold',
+                      'size': 10,
+                      'horizontalalignment': 'center',})
+        plt.savefig(temp_dir / ("data_frame_%03i.png"%i), dpi=100)
+
+    # Animate the frames into a gif:
+    palette = temp_dir / "palette2.png"
+    filters = "scale=trunc(iw/2)*2:trunc(ih/2)*2:flags=lanczos"
+    print("Converting pngs to gif...", end=' ')
+    convert_command_1 = [
+        'ffmpeg',
+        '-f', 'image2',
+        '-i', str(temp_dir / 'data_frame_%3d.png'),
+        '-vf', filters + ",palettegen",
+        '-y', palette]
+    convert_command_2 = [
+        'ffmpeg',
+        '-framerate', '12',
+        '-f', 'image2',
+        '-i', str(temp_dir / 'data_frame_%3d.png'),
+        '-i', palette,
+        '-lavfi', filters + " [x]; [x][1:v] paletteuse",
+        '-y', str(output_dir / "2_data_animation.gif")]
+    for convert_command in convert_command_1, convert_command_2:
+        try:
+            with open(temp_dir / 'conversion_messages.txt', 'wt') as f:
+                f.write("So far, everthing's fine...\n")
+                f.flush()
+                subprocess.check_call(convert_command, stderr=f, stdout=f)
+                f.flush()
+            (temp_dir / 'conversion_messages.txt').unlink()
+        except: # This is unlikely to be platform independent :D
+            print("GIF conversion failed. Is ffmpeg installed?")
+            raise
+    print('done.')
+
 
 def decode_timestamps(image_stack):
     """Decode PCO image timestamps from binary-coded decimal.
@@ -143,5 +261,10 @@ def adjust_contrast(img, min_, max_):
     img -= min_
     img /= max_ - min_
     return img
+
+def to_photoelectrons(camera_counts):
+    """A good approximation for the pco edge 4.2
+    """
+    return (camera_counts - 100) * 0.46
 
 main()
