dec7cff2cc70e8a0641e5b843b6bd9318faed497
Andrew York <andrew.g.york+github@gmail.com>
2021-02-03 14:16:40 -0800

    Update concept_animation_figure.py

--- concept_animation_figure.py
+++ concept_animation_figure.py
@@ -20,148 +20,37 @@ temp_dir.mkdir(exist_ok=True)
 output_dir.mkdir(exist_ok=True)
 
 def main():
-    # Load sprites
-    active_unbound_icon   = imread(input_dir / 'au.tif')
-    active_bound_icon     = imread(input_dir / 'ab.tif')
-    inactive_unbound_icon = imread(input_dir / 'iu.tif')
-    inactive_bound_icon   = imread(input_dir / 'ib.tif')
-    def pick_sprite(active, bound):
-        if active:
-            if bound:
-                return active_bound_icon
-            else:
-                return active_unbound_icon
-        else:
-            if bound:
-                return inactive_bound_icon
-            else:
-                return inactive_unbound_icon
-    
-
-    sensors_1 = SwitchingSensors(
+    sensor_params = dict(
         num_molecules=1000,
         initially_active=False,
-        initially_bound=False,
         # Odds of activation/deactivation:
         p_activation_bound=    0.005,
         p_activation_unbound=  0.125,
         p_deactivation_bound=  0.001,
-        p_deactivation_unbound=0.030,
-        # Odds of binding/unbinding:
-        p_binding_active=0,
-        p_binding_inactive=0,
-        p_unbinding_active=0,
-        p_unbinding_inactive=0)
-    
-    sensors_2 = SwitchingSensors(
-        num_molecules=1000,
-        initially_active=False,
-        initially_bound=True,
-        # Odds of activation/deactivation:
-        p_activation_bound=    0.005,
-        p_activation_unbound=  0.125,
-        p_deactivation_bound=  0.001,
-        p_deactivation_unbound=0.030,
-        # Odds of binding/unbinding:
-        p_binding_active=0,
-        p_binding_inactive=0,
-        p_unbinding_active=0,
-        p_unbinding_inactive=0)
+        p_deactivation_unbound=0.045)
+    sensors_1 = SwitchingSensors(initially_bound=False, **sensor_params)
+    sensors_2 = SwitchingSensors(initially_bound=True,  **sensor_params)
 
     # Remember a little bit of the simulation history:
     emissions_1 = [sensors_1.expected_emissions('average')]
     emissions_2 = [sensors_2.expected_emissions('average')]
-    # Start time-evolving the simulation and saving animation frames:
-    num_frames = 110
-    light_off_frame = 25
-    for i in range(num_frames):
-        # Figure generation
-        fig_aspect = 2
-        fig = plt.figure(figsize=(fig_aspect*5, 5))
-        ax0 = plt.axes([0.04 + 0.5/fig_aspect, 0.43, 0.94 - 1/fig_aspect, 0.57],
-                       frameon=False)
-        ax0.set_xticks([])
-        ax0.set_yticks([])
-        for im, x, y in ((active_bound_icon,     0.65, 0.75),
-                         (active_unbound_icon,   0.35, 0.75),
-                         (inactive_bound_icon,   0.65, 0.25),
-                         (inactive_unbound_icon, 0.35, 0.25)):
-            ax0.add_artist(AnnotationBbox(
-                OffsetImage(im, zoom=1.5),
-                (x, y), xycoords='data', frameon=False))
-        arrow_params = dict(linewidth=1,
-                            head_width=0.02,
-                            head_length=0.025,
-                            length_includes_head=True,
-                            edgecolor='black',
-                            facecolor='black',
-                            shape='right')
-        # Arrows for (un)binding rates:
-        ax0.arrow(0.45, 0.26,  0.10, 0, **arrow_params)
-        ax0.arrow(0.55, 0.24, -0.10, 0, **arrow_params)
-        ax0.arrow(0.45, 0.76,  0.10, 0, **arrow_params)
-        ax0.arrow(0.55, 0.74, -0.10, 0, **arrow_params)
-        # Arrows for (de)activation rates:
-        ax0.arrow(0.36, 0.5625,  0, -0.125, **arrow_params)
-        ax0.arrow(0.66, 0.5250,  0, -0.075, **arrow_params)
-        if 0 < i < light_off_frame:
-            arrow_params['facecolor'] = (0, 0.9, 0.9, 0.6)
-            arrow_params['edgecolor'] = (0, 0.9, 0.9, 0.6)
-            arrow_params['linewidth'] = 2.5
-            ax0.arrow(0.34, 0.375,  0, 0.25, **arrow_params)
-            ax0.arrow(0.64, 0.425,  0, 0.15, **arrow_params)
-
-        ax0.set_xlim(0, 1)
-        ax0.set_ylim(0, 1)
-        ax1 = plt.axes([0.005, 0.01, 0.5/fig_aspect, 0.98],
-                       frameon=True)
-        ax2 = plt.axes([0.995 - 0.5/fig_aspect, 0.01, 0.5/fig_aspect, 0.98],
-                        frameon=True)
-        for ax, sensors, color, linestyle in (
-            (ax1, sensors_1, 'C0', 'solid'),
-            (ax2, sensors_2, 'C1', (0, (0.02, 2)))):
-            ax.set_xticks([])
-            ax.set_yticks([])
-            for sp in ax.spines.values():
-                sp.set_color(color)
-                sp.set_linewidth(6)
-                sp.set_linestyle(linestyle)
-            for m in range(sensors.num_molecules):
-                im = pick_sprite(sensors.active[m], sensors.bound[m])
-                im = rotate(im, sensors.angles[m], reshape=False)
-                im = AnnotationBbox(
-                    OffsetImage(im, zoom=0.3),
-                    (sensors.x_positions[m], sensors.y_positions[m]),
-                    xycoords='data',
-                    frameon=False)
-                ax.add_artist(im)
-        ax3 = plt.axes([0.045 +0.5/fig_aspect, 0.07, 0.93 -1/fig_aspect, 0.35],
-                       frameon=True)
-        ax3.add_patch(Rectangle( # Show when the illumination is on:
-            (1, 0), light_off_frame-2, 1.2,
-            fill=True, linewidth=0, color=(0, 0.9, 0.9, 0.2)))
-
-        norm_1 = 0.792289 * sensors_1.num_molecules
-        norm_2 = 0.112073 * sensors_2.num_molecules
-        ax3.plot(np.array(emissions_1) / norm_1,
-                 linewidth=2)
-        ax3.plot(np.array(emissions_2) / norm_2,
-                 linewidth=3, linestyle=(0, (0.5, 0.6)))
-        ax3.set_xticks(np.linspace(0, num_frames, 7))
-        ax3.set_xticklabels(['' for x in ax3.get_xticklabels()])
-        ax3.set_yticks(np.linspace(0, 1, 5))
-        ax3.set_yticklabels(['' for x in ax3.get_yticklabels()])
-        ax3.set_xlim(0, num_frames - 31)
-        ax3.set_ylim(0, 1.2)
-        ax3.set_xlabel("Time")
-        ax3.set_ylabel("Normalized signal")
-        ax3.grid('on', alpha=0.1)
-       
-        plt.savefig(temp_dir / ('animation_frame_%06i.png'%(i+30)))
-        plt.close()
-
+    # Time-evolve the simulation and save animation frames:
+    initial_pause_frames = 30
+    num_frames = 150
+    light_off_frame = 55
+    for which_frame in range(initial_pause_frames, num_frames):
+        # Animation
+        save_animation_frame(
+            which_frame=which_frame,
+            initial_pause_frames=initial_pause_frames,
+            light_off_frame=light_off_frame,
+            num_frames=num_frames,
+            sensors_1=sensors_1,
+            sensors_2=sensors_2,
+            emissions_1=emissions_1,
+            emissions_2=emissions_2)
         # Simulation
-        if i == (light_off_frame-1):
+        if which_frame == light_off_frame:
             for s in sensors_1, sensors_2:
                 s.p_activation_bound   = 0
                 s.p_activation_unbound = 0
@@ -170,18 +59,17 @@ def main():
         emissions_1.append(sensors_1.expected_emissions('average'))
         emissions_2.append(sensors_2.expected_emissions('average'))
         print('.', end='')
-    for i in range(30):
+    # A long pause at the beginning:
+    for which_frame in range(initial_pause_frames):
         shutil.copyfile(
-            temp_dir / ('animation_frame_%06i.png'%30),
-            temp_dir / ('animation_frame_%06i.png'%i))
-##        shutil.copyfile(
-##            temp_dir / ('animation_frame_%06i.png'%(29+num_frames)),
-##            temp_dir / ('animation_frame_%06i.png'%(30+num_frames + i)))
+            temp_dir / ('animation_frame_%06i.png'%initial_pause_frames),
+            temp_dir / ('animation_frame_%06i.png'%which_frame))
+    # Lazy way of normalizing the curves:
     print()
     print("norm_1:", np.array(emissions_1).max() / sensors_1.num_molecules)
     print("norm_2:", np.array(emissions_2).max() / sensors_2.num_molecules)
-
-
+    # Convert from png to mp4:
+    make_mp4()
 
 class SwitchingSensors:
     """
@@ -219,8 +107,11 @@ class SwitchingSensors:
         self.active = np.full((num_molecules,), initially_active, dtype='bool')
         self.bound  = np.full((num_molecules,), initially_bound,  dtype='bool')
         # Average behavior:
-        self.expected_active_fraction = float(initially_active)
-        self.expected_bound_fraction  = float(initially_bound)
+        a, b, n = self.active, self.bound, self.num_molecules
+        self.avg_active_bound_fraction     = np.count_nonzero( a &  b) / n
+        self.avg_inactive_bound_fraction   = np.count_nonzero(~a &  b) / n
+        self.avg_active_unbound_fraction   = np.count_nonzero( a & ~b) / n
+        self.avg_inactive_unbound_fraction = np.count_nonzero(~a & ~b) / n
         # Odds of activation/deactivation:
         self.p_activation_bound     = p_activation_bound
         self.p_activation_unbound   = p_activation_unbound
@@ -279,24 +170,32 @@ class SwitchingSensors:
         #####################################
         ###### Update average state    ######
         #####################################
-        # Shorthand for the four state fractions:
-        a, b = self.expected_active_fraction, self.expected_bound_fraction
-        ab =    a  *    b
-        au =    a  * (1-b)
-        ib = (1-a) *    b
-        iu = (1-a) * (1-b)
-        # How does binding change, on average?
-        self.expected_bound_fraction += (
-            - self.p_unbinding_active   * ab  # Unbinding
-            + self.p_binding_active     * au  # Binding
-            - self.p_unbinding_inactive * ib  # Unbinding
-            + self.p_binding_inactive   * iu) # Binding
-        # How does activation change, on average?
-        self.expected_active_fraction += (
-            - self.p_deactivation_bound   * ab  # Deactivation
-            - self.p_deactivation_unbound * au  # Deactivation
-            + self.p_activation_bound     * ib  # Activation
-            + self.p_activation_unbound   * iu) # Activation
+        # Copies of the four state fractions w/short variable names:
+        ab = float(self.avg_active_bound_fraction)
+        ib = float(self.avg_inactive_bound_fraction)
+        au = float(self.avg_active_unbound_fraction)
+        iu = float(self.avg_inactive_unbound_fraction)
+        # Four updates, each depends on four probabilities and three fractions:
+        self.avg_active_bound_fraction += (
+            + ib * self.p_activation_bound     # Activating
+            - ab * self.p_deactivation_bound   # Deactivating
+            + au * self.p_binding_active       # Binding
+            - ab * self.p_unbinding_active)    # Unbinding
+        self.avg_inactive_bound_fraction += (
+            - ib * self.p_activation_bound     # Activating
+            + ab * self.p_deactivation_bound   # Deactivating
+            + iu * self.p_binding_inactive     # Binding
+            - ib * self.p_unbinding_inactive)  # Unbinding
+        self.avg_active_unbound_fraction += (
+            + iu * self.p_activation_unbound   # Activating
+            - au * self.p_deactivation_unbound # Deactivating
+            - au * self.p_binding_active       # Binding
+            + ab * self.p_unbinding_active)    # Unbinding
+        self.avg_inactive_unbound_fraction += (
+            - iu * self.p_activation_unbound   # Activating
+            + au * self.p_deactivation_unbound # Deactivating
+            - iu * self.p_binding_inactive     # Binding
+            + ib * self.p_unbinding_inactive)  # Unbinding
         return None
 
     def num_active(self):
@@ -316,17 +215,181 @@ class SwitchingSensors:
         """On average, how many photons do we expect to emit?
         """
         if state == 'average': # Calculate from average behavior
-            a = self.expected_active_fraction
-            b = self.expected_bound_fraction
+            ab = self.avg_active_bound_fraction
+            ib = self.avg_inactive_bound_fraction
+            au = self.avg_active_unbound_fraction
+            iu = self.avg_inactive_unbound_fraction
         elif state == 'stochastic': # Calculate from stochastic behavior
-            a = self.num_active() / self.num_molecules
-            b = self.num_bound() / self.num_molecules
+            a, b, n = self.active, self.bound, self.num_molecules
+            ab = np.count_nonzero( a &  b) / n
+            ib = np.count_nonzero(~a &  b) / n
+            au = np.count_nonzero( a & ~b) / n
+            iu = np.count_nonzero(~a & ~b) / n
         return self.num_molecules * (
-            (  a) * (  b) * self.active_bound_brightness +
-            (  a) * (1-b) * self.active_unbound_brightness +
-            (1-a) * (  b) * self.inactive_bound_brightness +
-            (1-a) * (1-b) * self.inactive_unbound_brightness)
-        
+            ab * self.active_bound_brightness +
+            au * self.active_unbound_brightness +
+            ib * self.inactive_bound_brightness +
+            iu * self.inactive_unbound_brightness)
+
+
+def save_animation_frame(
+    which_frame,
+    initial_pause_frames,    
+    light_off_frame,
+    num_frames,
+    sensors_1,
+    sensors_2,
+    emissions_1,
+    emissions_2,
+    ):
+    # Load sprites
+    active_unbound_icon   = imread(input_dir / 'au.tif')
+    active_bound_icon     = imread(input_dir / 'ab.tif')
+    inactive_unbound_icon = imread(input_dir / 'iu.tif')
+    inactive_bound_icon   = imread(input_dir / 'ib.tif')
+    def pick_sprite(active, bound):
+        if active:
+            if bound:
+                return active_bound_icon
+            else:
+                return active_unbound_icon
+        else:
+            if bound:
+                return inactive_bound_icon
+            else:
+                return inactive_unbound_icon
+
+    # Initialize figure
+    fig_aspect = 2
+    fig = plt.figure(figsize=(fig_aspect*5, 5))
+
+    # Panel for state transition diagram
+    ax0 = plt.axes([0.04 + 0.5/fig_aspect, 0.43, 0.94 - 1/fig_aspect, 0.57],
+                   frameon=False)
+    ax0.set_xticks([])
+    ax0.set_yticks([])
+    ax0.text(0.11, 0.72, "Active,\nunbound")
+    ax0.text(0.11, 0.22, "Inactive,\nunbound")
+    ax0.text(0.75, 0.72, "Active,\nbound")
+    ax0.text(0.75, 0.22, "Inactive,\nbound")
+    for im, x, y in ((active_bound_icon,     0.65, 0.75),
+                     (active_unbound_icon,   0.35, 0.75),
+                     (inactive_bound_icon,   0.65, 0.25),
+                     (inactive_unbound_icon, 0.35, 0.25)):
+        ax0.add_artist(AnnotationBbox(
+            OffsetImage(im, zoom=1.5),
+            (x, y), xycoords='data', frameon=False))
+    arrow_params = dict(linewidth=1,
+                        head_width=0.02,
+                        head_length=0.025,
+                        length_includes_head=True,
+                        edgecolor='black',
+                        facecolor='black',
+                        shape='right')
+    # Arrows for (un)binding rates:
+    ax0.arrow(0.45, 0.26,  0.10, 0, **arrow_params)
+    ax0.arrow(0.55, 0.24, -0.10, 0, **arrow_params)
+    ax0.arrow(0.45, 0.76,  0.10, 0, **arrow_params)
+    ax0.arrow(0.55, 0.74, -0.10, 0, **arrow_params)
+    # Arrows for (de)activation rates:
+    ax0.arrow(0.36, 0.5625,  0, -0.125, **arrow_params)
+    ax0.arrow(0.66, 0.5250,  0, -0.075, **arrow_params)
+    if initial_pause_frames < which_frame <= light_off_frame:
+        arrow_params['facecolor'] = (0, 0.9, 0.9, 0.6)
+        arrow_params['edgecolor'] = (0, 0.9, 0.9, 0.6)
+        arrow_params['linewidth'] = 2.5
+        ax0.arrow(0.34, 0.375,  0, 0.25, **arrow_params)
+        ax0.arrow(0.64, 0.425,  0, 0.15, **arrow_params)
+        ax0.text(0.17, 0.485, "Photoswitching", weight='bold', ha='center',
+                 color=(0, 0.9, 0.9, 1))
+    if which_frame > light_off_frame:
+        ax0.text(0.17, 0.485, "Relaxation", weight='bold', ha='center')
+    ax0.set_xlim(0, 1)
+    ax0.set_ylim(0, 1)
+
+    # Panels for single-molecule photoswitching animations
+    ax1 = plt.axes([0.005, 0.01, 0.5/fig_aspect, 0.93],
+                   frameon=True)
+    ax2 = plt.axes([0.995 - 0.5/fig_aspect, 0.01, 0.5/fig_aspect, 0.93],
+                    frameon=True)
+    ax1.set_title("100% unbound")
+    ax2.set_title("100% bound")
+    for ax, sensors, color, linestyle in (
+        (ax1, sensors_1, 'C0', 'solid'),
+        (ax2, sensors_2, 'C1', (0, (0.02, 2)))):
+        ax.set_xticks([])
+        ax.set_yticks([])
+        for sp in ax.spines.values():
+            sp.set_color(color)
+            sp.set_linewidth(6)
+            sp.set_linestyle(linestyle)
+        for m in range(sensors.num_molecules):
+            im = pick_sprite(sensors.active[m], sensors.bound[m])
+            im = rotate(im, sensors.angles[m], reshape=False)
+            im = AnnotationBbox(
+                OffsetImage(im, zoom=0.3),
+                (sensors.x_positions[m], sensors.y_positions[m]),
+                xycoords='data',
+                frameon=False)
+            ax.add_artist(im)
+
+    # Panel for activation curves
+    ax3 = plt.axes([0.045 +0.5/fig_aspect, 0.07, 0.93 -1/fig_aspect, 0.35],
+                   frameon=True)
+    ax3.add_patch(Rectangle( # Show when the illumination is on:
+        (1, 0), light_off_frame-initial_pause_frames-1, 1.2,
+        fill=True, linewidth=0, color=(0, 0.9, 0.9, 0.2)))
+
+    norm_1 = 0.7283212 * sensors_1.num_molecules
+    norm_2 = 0.1164006 * sensors_2.num_molecules
+    ax3.plot(np.array(emissions_1) / norm_1,
+             linewidth=2,
+             label="100% unbound")
+    ax3.plot(np.array(emissions_2) / norm_2,
+             linewidth=3, linestyle=(0, (0.5, 0.6)),
+             label="100% bound")
+    ax3.set_xticks(np.arange(0, num_frames, 25))
+    ax3.set_xticklabels(['' for x in ax3.get_xticklabels()])
+    ax3.set_yticks(np.linspace(0, 1, 5))
+    ax3.set_yticklabels(['' for x in ax3.get_yticklabels()])
+    ax3.set_xlim(0, num_frames - initial_pause_frames - 30)
+    ax3.set_ylim(0, 1.2)
+    ax3.set_xlabel("Time")
+    ax3.set_ylabel("Normalized signal")
+    ax3.legend(loc=(0.585, 0.41))
+    ax3.grid('on', alpha=0.1)
+   
+    plt.savefig(temp_dir / ('animation_frame_%06i.png'%(which_frame)),
+                dpi=150)
+    plt.close()
+    return None        
+
+def make_mp4():
+    ## Make video from images
+    print("Converting images to mp4...", end='')
+    convert_command = [
+       'ffmpeg', '-y',            # auto overwrite files
+       '-r', '20',                # frame rate
+       '-f', 'image2',            # format is image sequence
+       '-i', temp_dir / 'animation_frame_%06d.png',  # image sequence name
+       '-movflags', 'faststart',  # internet optimisation(?)
+       '-pix_fmt', 'yuv420p',     # cross browser compatibility
+       '-vcodec', 'libx264',      # codec choice
+       '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',#even pixel number (important)
+       '-preset', 'veryslow',     # take time and compress to max
+       '-crf', '25',              # image quality vs file size
+       output_dir / 'figure.mp4'] # output file name
+    try:
+       with open(temp_dir / 'conversion_messages.txt', 'wt') as f:
+           f.write("So far, everthing's fine...\n")
+           f.flush()
+           subprocess.check_call(convert_command, stderr=f, stdout=f)
+           f.flush()
+       (temp_dir / 'conversion_messages.txt').unlink()
+    except: # This is unlikely to be platform independent :D
+       print("MP4 conversion failed. Is ffmpeg installed?")
+       raise
+    print('done.')
 
 if __name__ == '__main__':
     main()
